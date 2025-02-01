b1feed2e95fdd217d51e95b7925408fca1d9c641
Neri.Jz <nerijz.g@gmail.com>
2025-01-31 23:38:25 -0600

    agregar función para añadir amigos y validar entrada de nombre

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+let nombreDelAmigo = [];
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        nombreDelAmigo.push(nombre);
+        document.getElementById("amigo").value = "";
+    }
+}
+
+
+
