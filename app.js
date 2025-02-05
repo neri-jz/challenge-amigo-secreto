a62d03cda1fc916ecd75a53e96a981b352306ec8
Neri.Jz <nerijz.g@gmail.com>
2025-02-05 01:13:56 -0600

    agregar función para capturar y validar la entrada de nombre de amigo

--- app.js
+++ app.js
@@ -3,6 +3,36 @@
 
 let nombreDelAmigo = [];
 
-function agregarAmigo(nombre) {
-    
+/* Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y
+ añadirlo a la lista de amigos creada anteriormente.
+
+Tareas específicas:
+
+
+
+Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector 
+para obtener el texto ingresado por el usuario.
+
+Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. 
+Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+
+Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos 
+usando el método.push().
+
+Limpiar el campo de entrada: Después de añadir el nombre, 
+restablecer el campo de texto a una cadena vacía. */
+
+function agregarAmigo() {
+    let nombreAgregado = document.getElementById("amigo").value;
+    console.log(nombreAgregado);
+    if (nombreAgregado === "") { 
+        alert("por favor, ingresa un nombre")
+    } else {
+        nombreDelAmigo.push(nombreAgregado);
+        document.getElementById("amigo").value = "";
+    }
+    return;
 }
+
+console.log(nombreDelAmigo);
+
